ef089933d15a47dbc347d8f7182d6178fffb8e0f
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2018-10-23 18:06:16 +0530

    testgcp

---
+++ t/lambda.js
@@ -0,0 +1,45 @@
+let AWS = require('aws-sdk');
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const storage = google.storage('v1');
+
+
+exports.handler = function (event, context, callback) {
+storage.objects.insert({
+  "bucket": "test_sigma_gcp_storage",
+  "name": "Ob2",
+  "media": {
+    "body": "Testing"
+  }
+})
+    .then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "kind": "storage#object",
+            "id": "<bucket>/<name>/<timestamp>",
+            "selfLink": "https://www.googleapis.com/storage/v1/b/<bucket>/o/<name>",
+            "name": "<name>",
+            "bucket": "<bucket>",
+            "contentType": "<content-type>",
+            "timeCreated": "<yyyy-MM-ddTHH:mm:ss.###Z>",
+            "updated": "<yyyy-MM-ddTHH:mm:ss.###Z>",
+            "size": "<bytes>",
+            "md5Hash": "<hash>",
+            "metadata": {
+                "<key1>": "<val1>",
+                "<key2>": "<val2>"
+            },
+            "crc32c": "<crc>",
+            "etag": "<etag>"
+            // , ...
+        }
+        */
+    })
+    .catch(err => {
+        console.log(err, err.stack); // an error occurred
+    });
+    
+
+    callback(null, 'Successfully executed');
+}
